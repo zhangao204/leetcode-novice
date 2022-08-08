64c49e88de9075a1804f3218f08a4e27662c03f4
chokeberry <aozhangchina@outlook.com>
2022-08-08 12:21:50 +0800

    1480

---
+++ src/LeetCode1480.java
@@ -0,0 +1,9 @@
+// 一维数组的动态和
+public class LeetCode1480 {
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i] + nums[i - 1];
+        }
+        return nums;
+    }
+}
